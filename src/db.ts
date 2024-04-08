9c73977eb86bab8c5b3518a50e8f85aee81f9389
yogyy <yogimr261@gmail.com>
2024-04-08 20:25:17 +0700

    feat: add kysely

---
+++ src/db.ts
@@ -0,0 +1,18 @@
+import { Pool } from "pg";
+import { Kysely, PostgresDialect } from "kysely";
+import { DB } from "./types";
+
+const dialect = new PostgresDialect({
+  pool: new Pool({
+    connectionString: process.env.DATABASE_URL,
+    max: 10,
+  }),
+});
+
+// Database interface is passed to Kysely's constructor, and from now on, Kysely
+// knows your database structure.
+// Dialect is passed to Kysely's constructor, and from now on, Kysely knows how
+// to communicate with your database.
+export const db = new Kysely<DB>({
+  dialect,
+});
---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+import type { ColumnType } from "kysely";
+
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>;
+
+export type Timestamp = ColumnType<Date, Date | string, Date | string>;
+
+export interface ElyUser {
+  created_at: Generated<Timestamp>;
+  email: string;
+  id: Generated<string>;
+  is_active: Generated<boolean>;
+  name: string;
+  password: string;
+  role: Generated<"developer" | "user" | "verified">;
+  updated_at: Generated<Timestamp>;
+  username: string;
+}
+
+export interface JWTPayload {
+  id: string;
+  email: string;
+  name: string;
+}
+
+export interface DB {
+  ely_user: ElyUser;
+}
